2e8b9446203198527944c505e0795491d0b4bb09
Danil <git@brcloud.io>
2024-11-21 19:05:59 +0200

    Fix: Metric/Logs scope fix

--- src/Bw.VaultDigest.Web/RepeaterService.cs
+++ src/Bw.VaultDigest.Web/RepeaterService.cs
@@ -13,32 +13,33 @@ public class Schedule
 }
 
 public class RepeaterService(
-    ILogger<RepeaterService> logger, 
-    IOptions<Schedule> options, 
+    ILogger<RepeaterService> logger,
+    IOptions<Schedule> options,
     DigestService digestSvc,
     MetricsFactory metricsFactory)
     : BackgroundService
 {
-    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         var cron = CrontabSchedule.Parse(options.Value.Cron);
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            using var _0 = logger.BeginScope(new Dictionary<string, object>{ { "RepeaterServiceExecutionId", Guid.NewGuid() } });
-            using var _1 = metricsFactory.CreateDurationMetric("repeater-run.duration");
-            
             logger.LogInformation("RepeaterService is active: Creating digest");
 
-            await digestSvc.CreateDigest();
+            using (logger.BeginScope(
+                       new Dictionary<string, object> { { "RepeaterServiceExecutionId", Guid.NewGuid() } }))
+            using (metricsFactory.CreateDurationMetric("repeater-run.duration"))
+            {
+                await digestSvc.CreateDigest();
 
-            logger.LogInformation("RepeaterService: Digest has been created");
+                logger.LogInformation("Digest has been created");
+            }
 
             var next = cron.GetNextOccurrence(DateTime.Now);
 
             logger.LogInformation("Scheduling next run for {Next}", next);
-            
+
             await Task.Delay(next - DateTime.Now, stoppingToken);
         }
     }
